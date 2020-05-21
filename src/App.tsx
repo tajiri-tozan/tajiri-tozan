9098d51d6cf76d4b2b71af6650738531fadb6398
naoto hamada <hamada@vis-its.com>
2020-05-22 00:11:56 +0900

    Add react-genie

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import { BrowserRouter } from "react-router-dom";
+import { ReactGenieAnimations } from "react-genie-styled-components";
 import "./App.css";
 import RootRouter from "pages";
 
 function App() {
   return (
     <BrowserRouter>
+      <ReactGenieAnimations />
       <RootRouter />
     </BrowserRouter>
   );
--- src/pages/Top/index.tsx
+++ src/pages/Top/index.tsx
@@ -1,5 +1,35 @@
 import React from "react";
+import { Reveal } from "react-genie";
+import { Animation } from "react-genie-styled-components";
+import styled from "styled-components/macro";
+
+const Contents = styled.div`
+  background: #151515 url("/images/yarigadake.jpg") no-repeat fixed center
+    bottom;
+  background-size: cover;
+  color: #fff;
+  text-align: center;
+`;
 
 export default function () {
-  return <div>top</div>;
+  return (
+    <Contents>
+      <Reveal animation={Animation.FadeInUp}>
+        <h1>Revealing this</h1>
+        <img src="/images/yarigadake.jpg" alt="槍ヶ岳" width="50%" />
+      </Reveal>
+      <Reveal animation={Animation.SlideInLeft}>
+        <h1>From The Left</h1>
+        <img src="/images/yarigadake.jpg" alt="槍ヶ岳" width="50%" />
+      </Reveal>
+      <Reveal animation={Animation.SlideInRight}>
+        <h1>From The Right</h1>
+        <img src="/images/yarigadake.jpg" alt="槍ヶ岳" width="50%" />
+      </Reveal>
+      <Reveal animation={Animation.FadeIn}>
+        <h1>Fade In</h1>
+        <img src="/images/yarigadake.jpg" alt="槍ヶ岳" width="50%" />
+      </Reveal>
+    </Contents>
+  );
 }
