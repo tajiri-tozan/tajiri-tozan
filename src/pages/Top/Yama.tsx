98708e1420b9a90a455e2c78d14a9aefb3f24085
naoto hamada <hamada@vis-its.com>
2020-05-23 23:33:11 +0900

    Add lazyload

--- src/pages/Top/Yama.tsx
+++ src/pages/Top/Yama.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Reveal } from "react-genie";
 import { Animation } from "react-genie-styled-components";
+import { LazyLoadImage } from "react-lazy-load-image-component";
 import { makeStyles, createStyles, Theme } from "@material-ui/core/styles";
 import Grid from "@material-ui/core/Grid";
 import Paper from "@material-ui/core/Paper";
@@ -47,7 +48,7 @@ export default function (props: Props) {
         <Paper className={classes.paper}>
           <Grid container spacing={2}>
             <Grid item className={classes.image}>
-              <img className={classes.img} alt={title} src={imgSrc} />
+              <LazyLoadImage className={classes.img} alt={title} src={imgSrc} />
             </Grid>
             <Grid item xs={12} sm container alignItems="center">
               <Grid item xs container direction="column" spacing={2}>
