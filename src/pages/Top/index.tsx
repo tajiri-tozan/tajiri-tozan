2ba82728503cf7ca1f9245ba4ebd86122387ab9d
naoto hamada <hamada@vis-its.com>
2020-05-24 14:19:00 +0900

    Fix layout (#11)

--- src/pages/Top/index.tsx
+++ src/pages/Top/index.tsx
@@ -1,6 +1,10 @@
 import React from "react";
 import styled from "styled-components/macro";
-import Yama from "./Yama";
+import { Theme, createStyles, makeStyles } from "@material-ui/core/styles";
+import GridList from "@material-ui/core/GridList";
+import GridListTile from "@material-ui/core/GridListTile";
+import GridListTileBar from "@material-ui/core/GridListTileBar";
+// import tileData from "./tileData";
 
 const Contents = styled.div`
   background: #151515 url("/images/yarigadake.jpg") no-repeat fixed center
@@ -20,27 +24,66 @@ const SiteTitle = styled.h1`
   font-size: 5rem;
 `;
 
+const tileData = [
+  {
+    img: "/images/20141207-jinbasan.jpg",
+    title: "陣馬山",
+    description: "2014/12/07",
+  },
+  {
+    img: "/images/20150920-yarigadake.jpg",
+    title: "槍ヶ岳",
+    description: "2015/09/20",
+  },
+  {
+    img: "/images/20180721-yatsugatake.jpg",
+    title: "八ヶ岳",
+    description: "2018/07/21",
+  },
+];
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    root: {
+      display: "flex",
+      flexWrap: "wrap",
+      justifyContent: "space-around",
+      overflow: "hidden",
+      maxWidth: "512px",
+      margin: "auto",
+    },
+    gridList: {
+      margin: "auto",
+    },
+  })
+);
+
 export default function () {
+  const classes = useStyles();
+
   return (
     <Contents>
       <Header>
         <SiteTitle>Tajiri Tozan</SiteTitle>
       </Header>
-      <Yama
-        title={"陣馬山"}
-        description="2014/12/07"
-        imgSrc="/images/20141207-jinbasan.jpg"
-      />
-      <Yama
-        title={"槍ヶ岳"}
-        description="2015/09/20"
-        imgSrc="/images/20150920-yarigadake.jpg"
-      />
-      <Yama
-        title={"八ヶ岳"}
-        description="2018/07/21"
-        imgSrc="/images/20180721-yatsugatake.jpg"
-      />
+      <div className={classes.root}>
+        <GridList
+          cellHeight={768}
+          className={classes.gridList}
+          cols={1}
+          spacing={100}
+        >
+          {tileData.map((tile) => (
+            <GridListTile key={tile.img}>
+              <img src={tile.img} alt={tile.title} />
+              <GridListTileBar
+                title={tile.title}
+                subtitle={<span>{tile.description}</span>}
+              />
+            </GridListTile>
+          ))}
+        </GridList>
+      </div>
     </Contents>
   );
 }
