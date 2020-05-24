7dac6b079ff94d78375b14cdaea60a053f331cbf
naoto hamada <hamada@vis-its.com>
2020-05-24 22:51:24 +0900

    Refactoring yamadata

---
+++ src/pages/Top/yama.ts
@@ -0,0 +1,17 @@
+export default [
+  {
+    img: "/images/20141207-jinbasan.jpg",
+    title: "陣馬山",
+    description: "2014/12/07",
+  },
+  {
+    img: "/images/20150920-yarigadake.jpg",
+    title: "槍ヶ岳",
+    description: "2015/09/20",
+  },
+  {
+    img: "/images/20180721-yatsugatake.jpg",
+    title: "八ヶ岳",
+    description: "2018/07/21",
+  },
+];
